ee340bed2e7ddcb5160896a68ea8439f3f2b3f5d
Kamal Singh <kamal.singh@exa.net.uk>
2024-06-11 01:21:50 +0100

    controller to enroll and leave society

--- internal/controllers/student_controllers.go
+++ internal/controllers/student_controllers.go
@@ -66,3 +66,201 @@ func ShowStudentDashboard(c echo.Context) error {
 		NotEnrolledSocieties: notEnrolledSocieties,
 	}))
 }
+
+func EnrollInSociety(c echo.Context) error {
+	jwtPayloadString := c.Get("jwtPayload").([]byte)
+	jwtPayload := utils.CustomJwtClaims{}
+	err := json.Unmarshal(jwtPayloadString, &jwtPayload)
+	if err != nil {
+		slog.Error(
+			"in unmarshal of json of jwt payload",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// get the url params
+	params := struct {
+		StudentID int `param:"studetnID"`
+		SoceityID int `param:"societyID"`
+	}{}
+	err = (&echo.DefaultBinder{}).BindPathParams(c, &params)
+	if err != nil {
+		slog.Error(
+			"in unmarshal of json of jwt payload",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	_, err = database.DBQueries.EnrollStudentInSociety(context.Background(), database.EnrollStudentInSocietyParams{
+		StudentID: int32(jwtPayload.PersonID),
+		SocietyID: int32(params.SoceityID),
+	})
+	if err != nil {
+		slog.Error(
+			"in enrolling student in a society",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	enrolledInSocieties, err := database.DBQueries.GetAllSocietiesStudentIsEnrolledIn(context.Background(), int32(jwtPayload.PersonID))
+	if err != nil {
+		slog.Error(
+			"in fetching enrolled societies",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	notEnrolledSocieties, err := database.DBQueries.GetAllSocietiesStudentIsNotEnrolledIn(context.Background(), int32(jwtPayload.PersonID))
+	if err != nil {
+		slog.Error(
+			"in fetching not enrolled societies",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+		Message:  "Enrolled in society successfully",
+		Closable: true,
+		Variant:  utils.AlertVariantSuccess,
+		Duration: 3000,
+	})).Write(c.Response().Writer)
+	return utils.Render(c, http.StatusOK, templates.StudentDashboard(templates.StudentDashboardParams{
+		JWTPayload:           jwtPayload,
+		EnrolledSocieties:    enrolledInSocieties,
+		NotEnrolledSocieties: notEnrolledSocieties,
+	}))
+}
+
+func LeaveSociety(c echo.Context) error {
+	jwtPayloadString := c.Get("jwtPayload").([]byte)
+	jwtPayload := utils.CustomJwtClaims{}
+	err := json.Unmarshal(jwtPayloadString, &jwtPayload)
+	if err != nil {
+		slog.Error(
+			"in unmarshal of json of jwt payload",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// get the url params
+	params := struct {
+		StudentID int `param:"studetnID"`
+		SoceityID int `param:"societyID"`
+	}{}
+	err = (&echo.DefaultBinder{}).BindPathParams(c, &params)
+	if err != nil {
+		slog.Error(
+			"in unmarshal of json of jwt payload",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	_, err = database.DBQueries.LeaveSociety(context.Background(), database.LeaveSocietyParams{
+		StudentID: int32(jwtPayload.PersonID),
+		SocietyID: int32(params.SoceityID),
+	})
+	if err != nil {
+		slog.Error(
+			"in leaving student in a society",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	enrolledInSocieties, err := database.DBQueries.GetAllSocietiesStudentIsEnrolledIn(context.Background(), int32(jwtPayload.PersonID))
+	if err != nil {
+		slog.Error(
+			"in fetching enrolled societies",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	notEnrolledSocieties, err := database.DBQueries.GetAllSocietiesStudentIsNotEnrolledIn(context.Background(), int32(jwtPayload.PersonID))
+	if err != nil {
+		slog.Error(
+			"in fetching not enrolled societies",
+			"error", err,
+		)
+		htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+			Message:  "Internal server error",
+			Closable: true,
+			Variant:  utils.AlertVariantDanger,
+			Duration: 3000,
+		})).Write(c.Response().Writer)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	htmx.NewResponse().AddTrigger(htmx.TriggerObject("alert", utils.AlertDetails{
+		Message:  "Left in society successfully",
+		Closable: true,
+		Variant:  utils.AlertVariantSuccess,
+		Duration: 3000,
+	})).Write(c.Response().Writer)
+	return utils.Render(c, http.StatusOK, templates.StudentDashboard(templates.StudentDashboardParams{
+		JWTPayload:           jwtPayload,
+		EnrolledSocieties:    enrolledInSocieties,
+		NotEnrolledSocieties: notEnrolledSocieties,
+	}))
+}
